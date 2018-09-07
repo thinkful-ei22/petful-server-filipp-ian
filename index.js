04a7eef1a8348fc690689ae3d6f85290d1bc1020
Ian Beihl <ianbeihl@gmail.com>
2018-09-07 08:07:08 -0700

    added get/delete endpoints

--- index.js
+++ index.js
@@ -10,6 +10,64 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+const fluffy = {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}
+const skittles = {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'yellow and green cat.',
+  name: 'Skittles',
+  sex: 'Female',
+  age: 2,
+  breed: 'tabby',
+  story: 'Thrown on the street'
+}
+const felix = {
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'siamese',
+  name: 'felix',
+  sex: 'Female',
+  age: 5,
+  breed: 'siamese',
+  story: 'Thrown on the street'
+}
+const bowser = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}
+const bandit = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'old pug.',
+  name: 'bandit',
+  sex: 'Male',
+  age: 3,
+  breed: 'pug',
+  story: 'Owner Passed away'
+}
+const wrigley = {
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'friendly buddy',
+  name: 'wrigley',
+  sex: 'Male',
+  age: 3,
+  breed: 'weiner dog',
+  story: 'Owner Passed away'
+}
+
+let dogs = [ bowser, bandit, wrigley ];
+let cats = [ fluffy, skittles, felix ];
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,6 +79,27 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+
+app.get('/api/cat', (req, res, next) => {
+  res.json(cats[0])
+})
+
+app.get('/api/dog', (req, res, next) => {
+  res.json(dogs[0])
+})
+
+app.delete('/api/cat', (req, res, next) => {
+  cats.shift();
+  res.status(204).end();
+})
+
+app.delete('/api/dog', (req, res, next) => {
+  dogs.shift();
+  res.status(204).end();
+})
+
+
 
 function runServer(port = PORT) {
   const server = app
