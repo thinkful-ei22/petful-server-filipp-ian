47e333e1aa6d955fc05053597d65dacab3c4d014
Ian Beihl <ianbeihl@gmail.com>
2018-09-07 12:46:19 -0700

    updated readme

--- queue.js
+++ queue.js
@@ -56,7 +56,7 @@ const fluffy = {
     story: 'Thrown on the street'
 }
 const skittles = {
-    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
     imageDescription: 'yellow and green cat.',
     name: 'Skittles',
     sex: 'Female',
@@ -65,7 +65,7 @@ const skittles = {
     story: 'Thrown on the street'
 }
 const felix = {
-    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageURL: 'https://r.hswstatic.com/w_907/gif/tesla-cat.jpg',
     imageDescription: 'siamese',
     name: 'felix',
     sex: 'Female',
@@ -83,7 +83,7 @@ const bowser = {
     story: 'Owner Passed away'
 }
 const bandit = {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsJ81ExajuASG5hIET3Wm2U_cnoACEKXLP7TDxf4TKZkvEXLCI',
     imageDescription: 'old pug.',
     name: 'bandit',
     sex: 'Male',
@@ -92,7 +92,7 @@ const bandit = {
     story: 'Owner Passed away'
 }
 const wrigley = {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageURL: 'https://hips.hearstapps.com/ghk.h-cdn.co/assets/18/12/beagle.jpg',
     imageDescription: 'friendly buddy',
     name: 'wrigley',
     sex: 'Male',
