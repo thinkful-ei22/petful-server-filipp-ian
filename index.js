2ec31d8e88f433a490bf45befd305bec8908fc14
Ian Beihl <ianbeihl@gmail.com>
2018-09-07 11:52:24 -0700

    added queue DS

--- index.js
+++ index.js
@@ -3,70 +3,14 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const { Queue } = require('./queue');
+let {catQueue, dogQueue} = require('./queue');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
-
-const fluffy = {
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
-}
-const skittles = {
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: 'yellow and green cat.',
-  name: 'Skittles',
-  sex: 'Female',
-  age: 2,
-  breed: 'tabby',
-  story: 'Thrown on the street'
-}
-const felix = {
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: 'siamese',
-  name: 'felix',
-  sex: 'Female',
-  age: 5,
-  breed: 'siamese',
-  story: 'Thrown on the street'
-}
-const bowser = {
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
-}
-const bandit = {
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'old pug.',
-  name: 'bandit',
-  sex: 'Male',
-  age: 3,
-  breed: 'pug',
-  story: 'Owner Passed away'
-}
-const wrigley = {
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'friendly buddy',
-  name: 'wrigley',
-  sex: 'Male',
-  age: 3,
-  breed: 'weiner dog',
-  story: 'Owner Passed away'
-}
-
-let dogs = [ bowser, bandit, wrigley ];
-let cats = [ fluffy, skittles, felix ];
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -82,20 +26,20 @@ app.use(
 
 
 app.get('/api/cat', (req, res, next) => {
-  res.json(cats[0])
+  res.json(catQueue.peek())
 })
 
 app.get('/api/dog', (req, res, next) => {
-  res.json(dogs[0])
+  res.json(dogQueue.peek())
 })
 
 app.delete('/api/cat', (req, res, next) => {
-  cats.shift();
+  catQueue.dequeue();
   res.status(204).end();
 })
 
 app.delete('/api/dog', (req, res, next) => {
-  dogs.shift();
+  dogQueue.dequeue();
   res.status(204).end();
 })
 
---
+++ queue.js
@@ -0,0 +1,117 @@
+class _Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
+
+    enqueue(data) {
+        const node = new _Node(data);
+
+        if (this.first === null) {
+            this.first = node;
+        }
+
+        if (this.last) {
+            node.next = this.last;
+            this.last.prev = node;
+        }
+
+        this.last = node;
+    }
+
+    dequeue() {
+        if (this.first === null) {
+            return;
+        }
+
+        const node = this.first;
+        this.first = node.prev;
+
+        if (node === this.last) {
+            this.last = null;
+        }
+
+        return node.value;
+    }
+    peek() {
+        return this.first.value;
+    }
+}
+
+const fluffy = {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+}
+const skittles = {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'yellow and green cat.',
+    name: 'Skittles',
+    sex: 'Female',
+    age: 2,
+    breed: 'tabby',
+    story: 'Thrown on the street'
+}
+const felix = {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'siamese',
+    name: 'felix',
+    sex: 'Female',
+    age: 5,
+    breed: 'siamese',
+    story: 'Thrown on the street'
+}
+const bowser = {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'bowser',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+}
+const bandit = {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'old pug.',
+    name: 'bandit',
+    sex: 'Male',
+    age: 3,
+    breed: 'pug',
+    story: 'Owner Passed away'
+}
+const wrigley = {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'friendly buddy',
+    name: 'wrigley',
+    sex: 'Male',
+    age: 3,
+    breed: 'weiner dog',
+    story: 'Owner Passed away'
+}
+
+
+let dogQueue = new Queue();
+dogQueue.enqueue(bowser);
+dogQueue.enqueue(bandit);
+dogQueue.enqueue(wrigley);
+
+let catQueue = new Queue();
+catQueue.enqueue(fluffy);
+catQueue.enqueue(skittles);
+catQueue.enqueue(felix);
+
+
+module.exports = {Queue, dogQueue, catQueue};
+
